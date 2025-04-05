a8b57bee60622a26339d14efb28b1bd8c2d79c70
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-04-05 16:47:04 -0300

    Add Falloff node

---
+++ addons/gaea/graph/nodes/root/data/generation/falloff_map.gd
@@ -0,0 +1,28 @@
+@tool
+extends GaeaNodeResource
+
+
+func get_data(_output_port: int, area: AABB, generator_data: GaeaData) -> Dictionary[Vector3i, float]:
+	var start: float = get_arg("start", generator_data)
+	var end: float = get_arg("end", generator_data)
+	var new_grid: Dictionary[Vector3i, float]
+
+	for x in get_axis_range(Axis.X, area):
+		for y in get_axis_range(Axis.Y, area):
+			var i: float = x / float(area.size.x) * 2 - 1
+			var j: float = y / float(area.size.y) * 2 - 1
+
+			# Get closest to 1.
+			var value: float = maxf(absf(i), absf(j))
+			var falloff_value: float
+
+			if value < start:
+				falloff_value = 1.0
+			elif value > end:
+				falloff_value = 0.0
+			else:
+				falloff_value = smoothstep(1.0, 0.0, inverse_lerp(start, end, value))
+
+			new_grid[Vector3i(x, y, 0)] = falloff_value
+
+	return new_grid
