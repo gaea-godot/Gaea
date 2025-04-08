52eb1cb1b007afbe824a2b3d7cad7b3bb9e12dd0
Zehir <zehir@zorim.fr>
2025-04-09 00:34:07 +0200

    code formatting

--- addons/gaea/graph/nodes/reroute_node_resource.gd
+++ addons/gaea/graph/nodes/reroute_node_resource.gd
@@ -3,9 +3,12 @@ extends GaeaNodeResource
 
 
 func get_data(
-	_passed_data: Array[Dictionary], _output_port: int, _area: AABB, _generator_data: GaeaData
+	passed_data: Array[Dictionary],
+	output_port: int,
+	_area: AABB,
+	_generator_data: GaeaData
 ) -> Dictionary:
-	return _passed_data[_output_port]
+	return passed_data[output_port]
 
 
 func get_type() -> GaeaGraphNode.SlotTypes:
