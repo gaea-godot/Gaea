78f99850de9e9e16b912e21b73a72f8a4604fa6e
Zehir <zehir@zorim.fr>
2025-04-12 21:31:25 +0200

    Add online docs button

--- addons/gaea/editor/panel.gd
+++ addons/gaea/editor/panel.gd
@@ -24,6 +24,7 @@ const _RerouteNode = preload("uid://bs40iof8ipbkq")
 @onready var _reload_node_tree_button: Button = %ReloadNodeTreeButton
 @onready var _reload_parameters_list_button: Button = %ReloadParametersListButton
 @onready var _file_dialog: FileDialog = $FileDialog
+@onready var _online_docs_button: Button = %OnlineDocsButton
 @onready var _window_popout_separator: VSeparator = %WindowPopoutSeparator
 @onready var _window_popout_button: Button = %WindowPopoutButton
 @onready var _bottom_note_label: RichTextLabel = %BottomNote
@@ -38,6 +39,7 @@ func _ready() -> void:
 	_save_button.icon = EditorInterface.get_base_control().get_theme_icon(&"Save", &"EditorIcons")
 	_load_button.icon = EditorInterface.get_base_control().get_theme_icon(&"Load", &"EditorIcons")
 	_window_popout_button.icon = EditorInterface.get_base_control().get_theme_icon(&"MakeFloating", &"EditorIcons")
+	_online_docs_button.icon = EditorInterface.get_base_control().get_theme_icon(&"ExternalLink", &"EditorIcons")
 	_create_node_panel.add_theme_stylebox_override(&"panel", EditorInterface.get_base_control().get_theme_stylebox(&"panel", &"PopupPanel"))
 
 	if not EditorInterface.is_multi_window_enabled():
@@ -480,3 +482,7 @@ func _get_multiwindow_support_tooltip_text() -> String:
 		return tr("Multi-window support is not available because the `--single-window` command line argument was used to start the editor.")
 	else:
 		return tr("Multi-window support is not available because the current platform doesn't support multiple windows.")
+
+
+func _on_online_docs_button_pressed() -> void:
+	OS.shell_open("https://gaea-godot.github.io/gaea-docs/#/")
