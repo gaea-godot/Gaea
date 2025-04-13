4620ef6173270f703cb0f11e6dbe7be0842f9c7f
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-04-13 17:24:03 -0300

    Change connection rim color

--- addons/gaea/editor/graph_edit.gd
+++ addons/gaea/editor/graph_edit.gd
@@ -12,6 +12,10 @@ func _init() -> void:
 	add_valid_connection_type(GaeaGraphNode.SlotTypes.RANGE, GaeaGraphNode.SlotTypes.VECTOR2)
 
 
+func _ready() -> void:
+	add_theme_color_override(&"connection_rim_color", Color("141414"))
+
+
 func _on_delete_nodes_request(nodes: Array[StringName]) -> void:
 	delete_nodes(nodes)
 
