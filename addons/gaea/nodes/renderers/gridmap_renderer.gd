8aa5d2baf380bb7a293b6692aa3350886e82020b
Zehir <zehir@zorim.fr>
2025-04-20 13:04:14 +0200

    Add missing methods in GridMapGaeaRenderer #325

--- addons/gaea/nodes/renderers/gridmap_renderer.gd
+++ addons/gaea/nodes/renderers/gridmap_renderer.gd
@@ -9,10 +9,21 @@ extends GaeaRenderer
 
 
 func _render(grid: GaeaGrid) -> void:
-	gridmap.clear()
+	_reset()
 
 	for layer_idx in grid.get_layers_count():
 		for cell in grid.get_layer(layer_idx):
 			var value = grid.get_layer(layer_idx)[cell]
 			if value is GridMapMaterial:
 				gridmap.set_cell_item(cell, value.item_idx)
+
+
+func _on_area_erased(area: AABB) -> void:
+	for x in range(area.position.x, area.end.x):
+		for y in range(area.position.y, area.end.y):
+			for z in range(area.position.z, area.end.z):
+				gridmap.set_cell_item(Vector3(x, y, z), GridMap.INVALID_CELL_ITEM)
+
+
+func _reset() -> void:
+	gridmap.clear()
