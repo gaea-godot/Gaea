3b24d3c0ec49a64e2f0bef2d58eaa19010f4a47d
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-05-23 19:04:34 -0300

    Update to_height_2d_and_3d.gd

--- testing/graph_nodes/to_height/to_height_2d_and_3d.gd
+++ testing/graph_nodes/to_height/to_height_2d_and_3d.gd
@@ -2,8 +2,8 @@ extends GdUnitTestSuite
 
 
 const AREA: AABB = AABB(Vector3.ZERO, Vector3(1, 4, 1) * 16)
-const EXPECTED_HASH_2D: int = 1603666212
-const EXPECTED_HASH_3D: int = 788074031
+const EXPECTED_HASH_2D: int = 3725516071
+const EXPECTED_HASH_3D: int = 2178371583
 
 var reference_data: Dictionary = {}
 var node: GaeaNodeToHeight
@@ -13,22 +13,34 @@ func before() -> void:
 	var noise: FastNoiseLite = FastNoiseLite.new()
 	for x in range(AREA.position.x, AREA.end.x):
 		for z in range(AREA.position.z, AREA.end.z):
-			reference_data[Vector3i(x, 0, z)] = noise.get_noise_3d(x, 0, z)
+			reference_data[Vector3i(x, 1, z)] = noise.get_noise_3d(x, 0, z)
 	node = GaeaNodeToHeight.new()
 	node.set_argument_value(&"reference_data", reference_data)
+	node.set_argument_value(&"reference_y", 1)
 
 
 func test_2d() -> void:
 	node.set_enum_value(0, GaeaNodeToHeight.Type.TYPE_2D)
 	node.set_argument_value(&"height_offset", -4)
-	assert_int(node._get_data(&"data", AREA, null).hash())\
+	var generated_data: Dictionary = node._get_data(&"data", AREA, null)
+	print(generated_data)
+	assert_dict(generated_data)\
+		.override_failure_message("Empty result from [b]GaeaNodeToHeight2D[/b].")\
+		.is_not_empty()
+	assert_int(generated_data.hash())\
 		.override_failure_message("Unexpected result from [b]GaeaNodeToHeight2D[/b].")\
+		.append_failure_message("Generated: %s\nExpected: %s" % [generated_data.hash(), EXPECTED_HASH_2D])\
 		.is_equal(EXPECTED_HASH_2D)
 
 
 func test_3d() -> void:
 	node.set_enum_value(0, GaeaNodeToHeight.Type.TYPE_3D)
 	node.set_argument_value(&"height_offset", 4)
-	assert_int(node._get_data(&"data", AREA, null).hash())\
+	var generated_data: Dictionary = node._get_data(&"data", AREA, null)
+	assert_dict(generated_data)\
+		.override_failure_message("Empty result from [b]GaeaNodeToHeight3D[/b].")\
+		.is_not_empty()
+	assert_int(generated_data.hash())\
 		.override_failure_message("Unexpected result from [b]GaeaNodeToHeight3D[/b].")\
+		.append_failure_message("Generated: %s\nExpected: %s" % [generated_data.hash(), EXPECTED_HASH_3D])\
 		.is_equal(EXPECTED_HASH_3D)
