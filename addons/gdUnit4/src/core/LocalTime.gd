02c9ecd5975e843662bca0e7d940278375b0e5cd
Zehir <zehir@zorim.fr>
2025-05-27 00:23:20 +0200

    Cleanup some warnings

--- addons/gdUnit4/src/core/LocalTime.gd
+++ addons/gdUnit4/src/core/LocalTime.gd
@@ -81,8 +81,11 @@ static func elapsed(p_time_ms :int) -> String:
 # create from epoch timestamp in ms
 func _init(time :int) -> void:
 	_time = time
+	@warning_ignore("integer_division")
 	_hour  =  (time / MILLIS_PER_HOUR) % 24
+	@warning_ignore("integer_division")
 	_minute =  (time / MILLIS_PER_MINUTE) % 60
+	@warning_ignore("integer_division")
 	_second =  (time / MILLIS_PER_SECOND) % 60
 	_millisecond = time % 1000
 
--- testing/generation/generate_test.gd
+++ testing/generation/generate_test.gd
@@ -7,7 +7,7 @@ var first_grid: GaeaGrid
 
 func test_has_generated() -> void:
 	var scene = load("uid://di7u4f3idjdd").instantiate()
-	var runner := scene_runner(scene)
+	var _runner := scene_runner(scene)
 	await scene.test_generation()
 	first_grid = scene.last_grid
 	assert_dict(first_grid._grid).is_not_empty()
@@ -15,7 +15,7 @@ func test_has_generated() -> void:
 
 func test_generations_match() -> void:
 	var scene = load("uid://di7u4f3idjdd").instantiate()
-	var runner := scene_runner(scene)
+	var _runner := scene_runner(scene)
 	await scene.test_generation()
 	var second_grid: GaeaGrid = scene.last_grid
 	assert_bool(first_grid._grid.recursive_equal(second_grid._grid, 1)).is_true()
@@ -23,7 +23,7 @@ func test_generations_match() -> void:
 
 func test_generations_dont_match() -> void:
 	var scene = load("uid://di7u4f3idjdd").instantiate()
-	var runner := scene_runner(scene)
+	var _runner := scene_runner(scene)
 	await scene.test_generation(1)
 	var second_grid: GaeaGrid = scene.last_grid
 	assert_bool(first_grid._grid.recursive_equal(second_grid._grid, 1)).is_false()
