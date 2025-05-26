9bc9a1a2e8b99ec38962e1cc7756c26806e0fb3c
Zehir <zehir@zorim.fr>
2025-05-27 00:10:28 +0200

    Fix sampler type

--- addons/gaea/graph/graph_nodes/root/resources/samplers/texture_sampler.gd
+++ addons/gaea/graph/graph_nodes/root/resources/samplers/texture_sampler.gd
@@ -34,15 +34,15 @@ func _get_output_port_type(_output_name: StringName) -> GaeaValue.Type:
 	return GaeaValue.Type.DATA
 
 
-func _get_data(output_port: StringName, area: AABB, graph: GaeaGraph) -> Dictionary[String, float]:
+func _get_data(output_port: StringName, area: AABB, graph: GaeaGraph) -> Dictionary[Vector3i, float]:
 	var texture: Texture = _get_arg(&"texture", area, graph)
 	if not is_instance_valid(texture):
 		return {}
 
-	var r_grid: Dictionary[String, float]
-	var g_grid: Dictionary[String, float]
-	var b_grid: Dictionary[String, float]
-	var alpha_grid: Dictionary[String, float]
+	var r_grid: Dictionary[Vector3i, float]
+	var g_grid: Dictionary[Vector3i, float]
+	var b_grid: Dictionary[Vector3i, float]
+	var alpha_grid: Dictionary[Vector3i, float]
 
 	var slices: Array[Image] # Only one is texture is 2D
 	if texture is Texture2D:
