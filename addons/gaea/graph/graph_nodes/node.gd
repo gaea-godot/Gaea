d0c4ef28330d93a5219d650cb4097cc8435959a3
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-09-28 14:30:06 -0300

    Update node.gd

--- addons/gaea/graph/graph_nodes/node.gd
+++ addons/gaea/graph/graph_nodes/node.gd
@@ -355,7 +355,7 @@ func _make_custom_tooltip(for_text: String) -> Object:
 
 	rich_text_label.bbcode_enabled = true
 	rich_text_label.text = GaeaNodeResource.get_formatted_text(for_text)
-	rich_text_label.text += "\n[right][b]Id: %s[/b][/right]" % resource.id
+	rich_text_label.text += "\n[right][b]ID: %s[/b][/right]" % resource.id
 	rich_text_label.fit_content = true
 	rich_text_label.custom_minimum_size.x = 256.0
 	return rich_text_label
