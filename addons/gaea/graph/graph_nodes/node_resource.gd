f1aace24e0e611ec0066383b09ca8b1d6238e5d1
Ian Cullum <cullumi@wwu.edu>
2025-10-04 21:52:25 -0700

    Decided to remove the added arg value logging. Useful, but there likely a better, more thorough way to do it.

--- addons/gaea/graph/graph_nodes/node_resource.gd
+++ addons/gaea/graph/graph_nodes/node_resource.gd
@@ -398,9 +398,7 @@ func _get_arg(arg_name: StringName, area: AABB, graph: GaeaGraph) -> Variant:
 			_log_error("Could not get data from previous node, using default value instead.", graph, connected_id)
 			return get_argument_default_value(arg_name)
 
-	var arg_value = arguments.get(arg_name, get_argument_default_value(arg_name))
-	_log_arg(arg_name, graph, arg_value)
-	return arg_value
+	return arguments.get(arg_name, get_argument_default_value(arg_name))
 #endregion
 
 
@@ -560,12 +558,9 @@ func _log_data(output_port: StringName, graph: GaeaGraph):
 
 
 # If enabled in [member GaeaGraph.logging], log the argument information. (See [enum GaeaGraph.Log]).
-func _log_arg(arg:String, graph: GaeaGraph, value:Variant = null):
+func _log_arg(arg:String, graph: GaeaGraph):
 	if is_instance_valid(graph) and graph.logging & GaeaGraph.Log.ARGS > 0:
-		if (value == null):
-			print("Arg       |   %s on %s" % [arg, _get_title()])
-		else:
-			print("Arg       |   %s on %s: %s" % [arg, _get_title(), value])
+		print("Arg       |   %s on %s" % [arg, _get_title()])
 
 
 ## Display a error message in the Output log panel.
