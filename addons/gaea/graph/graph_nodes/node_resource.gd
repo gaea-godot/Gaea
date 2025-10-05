e054be500acb227c1d95833abe29d8a3f7c4b58c
Zehir <zehir@zorim.fr>
2025-10-05 15:07:01 +0200

    Add data type in cache loading for traverse method

--- addons/gaea/graph/graph_nodes/node_resource.gd
+++ addons/gaea/graph/graph_nodes/node_resource.gd
@@ -59,7 +59,7 @@ var rng: RandomNumberGenerator
 ## Id in the [GaeaGraph] save data.
 var id: int = 0
 ## If empty, [method _get_title] will be used instead.
-var tree_name_override: String = "" : set = set_tree_name_override
+var tree_name_override: String = "": set = set_tree_name_override
 @export_storage var default_value_overrides: Dictionary[StringName, Variant]
 @export_storage var default_enum_value_overrides: Dictionary[int, int]
 
@@ -366,11 +366,6 @@ func _on_argument_value_changed(arg_name: StringName, new_value: Variant) -> voi
 	return
 
 
-
-
-
-
-
 #region Args
 ## Returns the value of the argument of [param name]. Pass in [param graph] to allow overriding with input slots.[br]
 ## [param area] is used for values of the type Data or Map. (See [enum GaeaValue.Type]).
@@ -412,7 +407,10 @@ func traverse(output_port: StringName, area: AABB, graph: GaeaGraph) -> Variant:
 	# Caching
 	var use_caching = _use_caching(output_port, graph)
 	if use_caching and _has_cached_data(output_port, graph):
-		return _get_cached_data(output_port, graph)
+		return {
+			&"value": _get_cached_data(output_port, graph),
+			&"type": get_output_port_type(output_port)
+		}
 
 	# Validation
 	if not _has_inputs_connected(_get_required_arguments(), graph):
@@ -448,8 +446,8 @@ func _use_caching(_output_port: StringName, _graph: GaeaGraph) -> bool:
 ## Adds or sets data to the cache at GaeaNodeResource, then output_port index.
 ## This is called during [method traverse] if [method _use_caching] returns [code]true[/code],
 ## but can also be called in special cases where you want to manually add cached values.
-func _set_cached_data(output_port: StringName, graph: GaeaGraph, new_data:Dictionary) -> void:
-	var node_cache:Dictionary = graph.cache.get_or_add(self, {})
+func _set_cached_data(output_port: StringName, graph: GaeaGraph, new_data: Variant) -> void:
+	var node_cache: Dictionary = graph.cache.get_or_add(self, {})
 	node_cache[output_port] = new_data
 
 
@@ -533,7 +531,7 @@ func connection_idx_to_output(output_idx: int) -> StringName:
 
 #region Logging
 # If enabled in [member GaeaGraph.logging], log the execution information. (See [enum GaeaGraph.Log]).
-func _log_execute(message:String, area:AABB, graph: GaeaGraph):
+func _log_execute(message: String, area: AABB, graph: GaeaGraph):
 	if is_instance_valid(graph) and graph.logging & GaeaGraph.Log.EXECUTE > 0:
 		message = message.strip_edges()
 		message = message if message == "" else message + " "
@@ -541,7 +539,7 @@ func _log_execute(message:String, area:AABB, graph: GaeaGraph):
 
 
 # If enabled in [member GaeaGraph.logging], log the layer information. (See [enum GaeaGraph.Log]).
-func _log_layer(message:String, layer:int, graph: GaeaGraph):
+func _log_layer(message: String, layer: int, graph: GaeaGraph):
 	if is_instance_valid(graph) and graph.logging & GaeaGraph.Log.EXECUTE > 0:
 		message = message.strip_edges()
 		message = message if message == "" else message + " "
@@ -561,7 +559,7 @@ func _log_data(output_port: StringName, graph: GaeaGraph):
 
 
 # If enabled in [member GaeaGraph.logging], log the argument information. (See [enum GaeaGraph.Log]).
-func _log_arg(arg:String, graph: GaeaGraph):
+func _log_arg(arg: String, graph: GaeaGraph):
 	if is_instance_valid(graph) and graph.logging & GaeaGraph.Log.ARGS > 0:
 		print("Arg       |   %s on %s" % [arg, _get_title()])
 
@@ -570,7 +568,7 @@ func _log_arg(arg:String, graph: GaeaGraph):
 ## If a [param node_idx] is provided, it will display the path and position of the node.
 ## Otherwise, it will display the path of the resource.
 ## The [param node_idx] is the index of the node in the graph.resources array.
-func _log_error(message:String, graph: GaeaGraph, node_idx: int = -1):
+func _log_error(message: String, graph: GaeaGraph, node_idx: int = -1):
 	if node_idx >= 0:
 		printerr("%s:%s in node '%s' - %s" % [
 			graph.get_node(node_idx).resource_path,
