d7962e24171891ad39140f197c51b4732c859be8
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-10-16 11:46:19 -0300

    Rename `DataSetOp` to `SampleSetOp`

--- addons/gaea/graph/graph_nodes/root/sampling/operations/sets/sample_set_operation.gd
+++ addons/gaea/graph/graph_nodes/root/sampling/operations/sets/sample_set_operation.gd
@@ -1,11 +1,11 @@
 @tool
-class_name GaeaNodeDataSetOp
+class_name GaeaNodeSampleSetOp
 extends GaeaNodeSetOp
 ## Sample version of [GaeaNodeSetOp].
 
 
 func _get_title() -> String:
-	return "DataSetOp"
+	return "SampleSetOp"
 
 
 func _get_output_port_type(_output_name: StringName) -> GaeaValue.Type:
