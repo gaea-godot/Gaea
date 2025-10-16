b636d57c87bc4ac1bc01aeb8df27ca914f205795
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-10-16 11:53:01 -0300

    Update editor settings to rename data-related settings

--- addons/gaea/editor/editor_settings.gd
+++ addons/gaea/editor/editor_settings.gd
@@ -28,7 +28,7 @@ const CONFIGURABLE_SLOT_COLORS := {
 	GaeaValue.Type.MAP: "map",
 }
 
-var editor_settings
+var editor_settings: EditorSettings
 
 
 func add_settings() -> void:
@@ -67,6 +67,7 @@ func add_settings() -> void:
 		{"type": TYPE_COLOR, "hint": PROPERTY_HINT_COLOR_NO_ALPHA}
 	)
 
+
 	for slot_type: GaeaValue.Type in CONFIGURABLE_SLOT_COLORS.keys():
 		_add_setting(
 			COLOR_BASE % CONFIGURABLE_SLOT_COLORS.get(slot_type),
@@ -81,6 +82,10 @@ func add_settings() -> void:
 			{"type": TYPE_STRING, "hint": PROPERTY_HINT_FILE, "hint_string": "*.png,*.jpg,*.svg"}
 		)
 
+	# Transfer data to sample since [#473](https://github.com/gaea-godot/gaea/pull/473).
+	_transfer_and_erase_setting(COLOR_BASE % "data", COLOR_BASE % "sample")
+	_transfer_and_erase_setting(ICON_BASE % "data", ICON_BASE % "sample")
+
 
 func _add_setting(key: String, default_value: Variant, property_info: Dictionary) -> void:
 	if not editor_settings.has_setting(key):
@@ -88,6 +93,12 @@ func _add_setting(key: String, default_value: Variant, property_info: Dictionary
 	editor_settings.set_initial_value(key, default_value, false)
 	property_info.set("name", key)
 	editor_settings.add_property_info(property_info)
+
+
+func _transfer_and_erase_setting(old_key: String, new_key: String) -> void:
+	if editor_settings.has_setting(old_key):
+		editor_settings.set_setting(new_key, editor_settings.get_setting(old_key))
+		editor_settings.erase(old_key)
 
 
 static func get_configured_output_color() -> Color:
