37237dcc618e5d6a7428cc2998ad9b7927228897
Ian Cullum <cullumi@wwu.edu>
2025-11-03 16:46:51 -0800

    Added more specifically typed renderer properties to gridmap_renderer_test.gd and tilemap_renderer_test.gd for better type checking

--- testing/rendering/gridmap_renderer_test.gd
+++ testing/rendering/gridmap_renderer_test.gd
@@ -9,6 +9,8 @@ const ITEM_2_TILES_HASH_SMALL := 3765593323
 
 var scene: GaeaRendererTester
 var runner: GdUnitSceneRunner
+var renderer: GridMapGaeaRenderer:
+	get: return scene.renderer
 
 
 func before() -> void:
@@ -18,9 +20,9 @@ func before() -> void:
 
 func test_full_area() -> void:
 	scene.generator.generate()
-	await scene.renderer.render_finished
+	await renderer.render_finished
 	
-	var item_1_cells_used: Array[Vector3i] = scene.renderer.grid_maps[0].get_used_cells_by_item(0)
+	var item_1_cells_used: Array[Vector3i] = renderer.grid_maps[0].get_used_cells_by_item(0)
 	assert_array(item_1_cells_used).is_not_empty()
 	var item_1_hash: int = item_1_cells_used.hash()
 	assert_int(item_1_hash)\
@@ -28,7 +30,7 @@ func test_full_area() -> void:
 		.append_failure_message("Produced hash:%s\n Expected hash:%s" % [item_1_hash, ITEM_1_TILES_HASH])\
 		.is_equal(ITEM_1_TILES_HASH)
 
-	var item_2_cells_used: Array[Vector3i] = scene.renderer.grid_maps[1].get_used_cells_by_item(1)
+	var item_2_cells_used: Array[Vector3i] = renderer.grid_maps[1].get_used_cells_by_item(1)
 	assert_array(item_2_cells_used).is_not_empty()
 	var item_2_hash: int = item_2_cells_used.hash()
 	assert_int(item_2_hash)\
@@ -39,10 +41,10 @@ func test_full_area() -> void:
 
 func test_reset() -> void:
 	scene.generator.request_reset()
-	assert_array(scene.renderer.grid_maps[0].get_used_cells())\
+	assert_array(renderer.grid_maps[0].get_used_cells())\
 		.override_failure_message("[b]GridMapGaeaRenderer[/b] wasn't cleared properly.")\
 		.is_empty()
-	assert_array(scene.renderer.grid_maps[1].get_used_cells())\
+	assert_array(renderer.grid_maps[1].get_used_cells())\
 		.override_failure_message("[b]GridMapGaeaRenderer[/b] wasn't cleared properly.")\
 		.is_empty()
 
@@ -52,7 +54,7 @@ func test_small_area() -> void:
 	scene.generator.generate_area(area)
 	await scene.generator.generation_finished
 
-	var item_1_cells_used: Array[Vector3i] = scene.renderer.grid_maps[0].get_used_cells_by_item(0)
+	var item_1_cells_used: Array[Vector3i] = renderer.grid_maps[0].get_used_cells_by_item(0)
 	assert_array(item_1_cells_used).is_not_empty()
 	var item_1_hash: int = item_1_cells_used.hash()
 	assert_int(item_1_hash)\
@@ -60,7 +62,7 @@ func test_small_area() -> void:
 		.append_failure_message("Produced hash:%s\n Expected hash:%s" % [item_1_hash, ITEM_1_TILES_HASH])\
 		.is_equal(ITEM_1_TILES_HASH_SMALL)
 
-	var item_2_cells_used: Array[Vector3i] = scene.renderer.grid_maps[1].get_used_cells_by_item(1)
+	var item_2_cells_used: Array[Vector3i] = renderer.grid_maps[1].get_used_cells_by_item(1)
 	assert_array(item_2_cells_used).is_not_empty()
 	var item_2_hash: int = item_2_cells_used.hash()
 	assert_int(item_2_hash)\
@@ -69,6 +71,6 @@ func test_small_area() -> void:
 		.is_equal(ITEM_2_TILES_HASH_SMALL)
 
 func test_null_layer() -> void:
-	scene.renderer.grid_maps[1] = null
+	renderer.grid_maps[1] = null
 	assert_error(scene.generator.generate)\
 		.is_success()
--- testing/rendering/tilemap_renderer_test.gd
+++ testing/rendering/tilemap_renderer_test.gd
@@ -9,7 +9,8 @@ const SAND_TILES_HASH_SMALL := 1850823263
 
 var scene: GaeaRendererTester
 var runner: GdUnitSceneRunner
-
+var renderer: TileMapGaeaRenderer:
+	get: return scene.renderer
 
 func before() -> void:
 	scene = load("uid://xfcy8wwh3sd7").instantiate()
@@ -18,15 +19,15 @@ func before() -> void:
 
 func test_full_area() -> void:
 	scene.generator.generate()
-	await scene.renderer.render_finished
+	await renderer.render_finished
 
-	var grass_hash: int = scene.renderer.tile_map_layers[0].get_used_cells_by_id(0, Vector2i(0, 0)).hash()
+	var grass_hash: int = renderer.tile_map_layers[0].get_used_cells_by_id(0, Vector2i(0, 0)).hash()
 	assert_int(grass_hash)\
 		.override_failure_message("Layer 0 rendering of [b]TileMapGaeaRenderer[/b] not working as expected.")\
 		.append_failure_message("Produced hash:%s\n Expected hash:%s" % [grass_hash, GRASS_TILES_HASH])\
 		.is_equal(GRASS_TILES_HASH)
 
-	var sand_hash: int = scene.renderer.tile_map_layers[1].get_used_cells_by_id(0, Vector2i(1, 0)).hash()
+	var sand_hash: int = renderer.tile_map_layers[1].get_used_cells_by_id(0, Vector2i(1, 0)).hash()
 	assert_int(sand_hash)\
 		.override_failure_message("Layer 1 rendering of [b]TileMapGaeaRenderer[/b] not working as expected.")\
 		.append_failure_message("Produced hash:%s\n Expected hash:%s" % [sand_hash, SAND_TILES_HASH])\
@@ -35,10 +36,10 @@ func test_full_area() -> void:
 
 func test_reset() -> void:
 	scene.generator.request_reset()
-	assert_array(scene.renderer.tile_map_layers[0].get_used_cells())\
+	assert_array(renderer.tile_map_layers[0].get_used_cells())\
 		.override_failure_message("[b]TileMapGaeaRenderer[/b] wasn't cleared properly.")\
 		.is_empty()
-	assert_array(scene.renderer.tile_map_layers[1].get_used_cells())\
+	assert_array(renderer.tile_map_layers[1].get_used_cells())\
 		.override_failure_message("[b]TileMapGaeaRenderer[/b] wasn't cleared properly.")\
 		.is_empty()
 
@@ -47,28 +48,29 @@ func test_small_area() -> void:
 	var area: AABB = AABB(Vector3.ZERO, Vector3.ONE * 16)
 	scene.generator.generate_area(area)
 	await scene.generator.generation_finished
+	
 
-	var grass_hash: int = scene.renderer.tile_map_layers[0].get_used_cells_by_id(0, Vector2i(0, 0)).hash()
+	var grass_hash: int = renderer.tile_map_layers[0].get_used_cells_by_id(0, Vector2i(0, 0)).hash()
 	assert_int(grass_hash)\
 		.override_failure_message("Layer 0 rendering of [b]TileMapGaeaRenderer[/b] area not working as expected.")\
 		.append_failure_message("Produced hash:%s\n Expected hash:%s" % [grass_hash, GRASS_TILES_HASH_SMALL])\
 		.is_equal(GRASS_TILES_HASH_SMALL)
 
-	var sand_hash: int = scene.renderer.tile_map_layers[1].get_used_cells_by_id(0, Vector2i(1, 0)).hash()
+	var sand_hash: int = renderer.tile_map_layers[1].get_used_cells_by_id(0, Vector2i(1, 0)).hash()
 	assert_int(sand_hash)\
 		.override_failure_message("Layer 1 rendering of [b]TileMapGaeaRenderer[/b] area not working as expected.")\
 		.append_failure_message("Produced hash:%s\n Expected hash:%s" % [sand_hash, SAND_TILES_HASH_SMALL])\
 		.is_equal(SAND_TILES_HASH_SMALL)
 
-	assert_vector(scene.renderer.tile_map_layers[0].get_used_rect().size)\
+	assert_vector(renderer.tile_map_layers[0].get_used_rect().size)\
 		.override_failure_message("Rendered [b]TileMapGaeaRenderer[/b] area was bigger than expected.")\
-		.is_less_equal(Vector2(area.size.x, area.size.y))
-	assert_vector(scene.renderer.tile_map_layers[1].get_used_rect().size)\
+		.is_less_equal(Vector2(area.size.x, area.size.y) as Vector2i)
+	assert_vector(renderer.tile_map_layers[1].get_used_rect().size)\
 		.override_failure_message("Rendered [b]TileMapGaeaRenderer[/b] area was bigger than expected.")\
-		.is_less_equal(Vector2(area.size.x, area.size.y))
+		.is_less_equal(Vector2(area.size.x, area.size.y) as Vector2i)
 
 
 func test_null_layer() -> void:
-	scene.renderer.tile_map_layers[1] = null
+	renderer.tile_map_layers[1] = null
 	assert_error(scene.generator.generate)\
 		.is_success()
