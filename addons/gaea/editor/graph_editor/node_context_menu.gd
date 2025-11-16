8a0c52bb4585e924686d42abd055bc866acabee5
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-11-16 14:03:33 -0300

    Fix conflicts and linting

--- addons/gaea/editor/graph_editor/node_context_menu.gd
+++ addons/gaea/editor/graph_editor/node_context_menu.gd
@@ -126,62 +126,8 @@ func _on_id_pressed(id: int) -> void:
 			if node is GaeaGraphFrame:
 				node.set_autoshrink_enabled(is_item_checked(idx))
 		Action.GROUP_IN_FRAME:
-			print("--------------------")
 			var selected: Array[StringName] = graph_edit.get_selected_names()
-			var front_node: StringName = selected.front()
-			var front_frame_tree: Array[StringName] = _get_node_frame_parents_list(front_node)
-			front_frame_tree.reverse()
-			# -1, D, A
-			prints("front_frame_tree", front_frame_tree)
-			for other_node: StringName in selected:
-				var other_frame_tree: Array[StringName] = _get_node_frame_parents_list(other_node)
-				other_frame_tree.reverse()
-				prints("other_frame_tree", other_node, other_frame_tree)
-				# -1, D, C, B
-				for i in range(0, mini(front_frame_tree.size(), other_frame_tree.size())):
-					if not front_frame_tree[i] == other_frame_tree[i]:
-						front_frame_tree.resize(i)
-
-			var matching_parent: StringName = front_frame_tree.back()
-			prints("matching_parent", matching_parent)
-			var things_to_group: Array[StringName] = []
-			var parent_name: StringName
-			for node_name: StringName in selected:
-				var loop_limit: int = 50
-				while loop_limit > 0:
-					loop_limit -= 1
-					parent_name = graph_edit.attached_elements.get(node_name, &"null")
-					if parent_name == matching_parent:
-						if not things_to_group.has(node_name):
-							things_to_group.append(node_name)
-						break
-					node_name = parent_name
-
-			prints("things_to_group", things_to_group)
-			var positions: Array = things_to_group.map(func(node_name: StringName): return (graph_edit.get_node(NodePath(node_name)) as GraphElement).position)
-			prints("positions", positions)
-			var frame_position: Vector2 = positions.reduce(func(a: Vector2, b: Vector2): return a.min(b), positions.front())
-			var new_frame_id: int = graph_edit.graph.add_frame(frame_position)
-			var new_frame: Node = graph_edit.instantiate_node(new_frame_id)
-			var new_frame_name: StringName = new_frame.name
-			prints("new frame", new_frame_name)
-
-			if matching_parent != &"null":
-				print("attach new frame %s to frame %s" % [new_frame_name, matching_parent])
-				graph_edit.attach_graph_element_to_frame(new_frame_name, matching_parent)
-				graph_edit._on_element_attached_to_frame(new_frame_name, matching_parent)
-
-				for node_name: StringName in things_to_group:
-					print("detach thing %s" % [node_name])
-					graph_edit.detach_element_from_frame(node_name)
-
-			for node_name in things_to_group:
-				print("attach thing %s" % [node_name])
-				graph_edit.attach_graph_element_to_frame(node_name, new_frame_name)
-				graph_edit._on_element_attached_to_frame(node_name, new_frame_name)
-
-			new_frame.selected = true
-
+			_group_nodes_in_frame(selected)
 
 		Action.DETACH:
 			var selected: Array = graph_edit.get_selected()
@@ -228,3 +174,52 @@ func _get_node_id(node: GraphElement) -> int:
 		return node.id
 
 	return -1
+
+
+func _group_nodes_in_frame(nodes: Array[StringName]) -> void:
+	var front_node: StringName = nodes.front()
+	var front_frame_tree: Array[StringName] = _get_node_frame_parents_list(front_node)
+	front_frame_tree.reverse()
+	for other_node: StringName in nodes:
+		var other_frame_tree: Array[StringName] = _get_node_frame_parents_list(other_node)
+		other_frame_tree.reverse()
+		for i in range(0, mini(front_frame_tree.size(), other_frame_tree.size())):
+			if not front_frame_tree[i] == other_frame_tree[i]:
+				front_frame_tree.resize(i)
+
+	var matching_parent: StringName = front_frame_tree.back()
+	var things_to_group: Array[StringName] = []
+	var parent_name: StringName
+	for node_name: StringName in nodes:
+		var loop_limit: int = 50
+		while loop_limit > 0:
+			loop_limit -= 1
+			parent_name = graph_edit.attached_elements.get(node_name, &"null")
+			if parent_name == matching_parent:
+				if not things_to_group.has(node_name):
+					things_to_group.append(node_name)
+				break
+			node_name = parent_name
+
+	var positions: Array = things_to_group.map(func(node_name: StringName):
+		return (graph_edit.get_node(NodePath(node_name)) as GraphElement).position
+	)
+	var frame_position: Vector2 = positions.reduce(func(a: Vector2, b: Vector2):
+		return a.min(b), positions.front()
+	)
+	var new_frame_id: int = graph_edit.graph.add_frame(frame_position)
+	var new_frame: Node = graph_edit.instantiate_node(new_frame_id)
+	var new_frame_name: StringName = new_frame.name
+
+	if matching_parent != &"null":
+		graph_edit.attach_graph_element_to_frame(new_frame_name, matching_parent)
+		graph_edit._on_element_attached_to_frame(new_frame_name, matching_parent)
+
+		for node_name: StringName in things_to_group:
+			graph_edit.detach_element_from_frame(node_name)
+
+	for node_name in things_to_group:
+		graph_edit.attach_graph_element_to_frame(node_name, new_frame_name)
+		graph_edit._on_element_attached_to_frame(node_name, new_frame_name)
+
+	new_frame.selected = true
