8156fed4ad37f3bb89b9178c89da6fd1554b18a0
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-11-16 11:43:20 -0300

    Fix crash
    Co-Authored-By: Zehir <845225+Zehir@users.noreply.github.com>

--- addons/gaea/editor/file_list/file_system_container.gd
+++ addons/gaea/editor/file_list/file_system_container.gd
@@ -144,6 +144,11 @@ func _on_item_selected(index: int) -> void:
 
 	graph_edit.unpopulate()
 	graph_edit.populate(metadata)
+	var edited_object = EditorInterface.get_inspector().get_edited_object()
+	if edited_object is GaeaGenerator:
+		if edited_object.graph == metadata:
+			return
+	EditorInterface.edit_resource(metadata)
 	EditorInterface.edit_resource(metadata)
 
 
